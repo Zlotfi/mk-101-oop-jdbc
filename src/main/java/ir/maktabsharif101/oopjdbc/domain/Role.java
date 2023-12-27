321212ba477f5ca29020c26956497a97082bf8c7
victus <zlotfi758@gmail.com>
2023-12-28 01:16:57 +0330

    add attribute, getter and setter method

---
+++ src/main/java/ir/maktabsharif101/oopjdbc/domain/Role.java
@@ -0,0 +1,33 @@
+package ir.maktabsharif101.oopjdbc.domain;
+
+@SuppressWarnings("unused")
+//@Entity
+public class Role {
+    private Long id;
+    private String name;
+    private Permission[] permissions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Permission[] getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Permission[] permissions) {
+        this.permissions = permissions;
+    }
+}
