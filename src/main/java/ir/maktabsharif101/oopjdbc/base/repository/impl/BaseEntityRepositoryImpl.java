b232ae7b25b11642bbd2a7645ebf2a2d4700ea23
victus <zlotfi758@gmail.com>
2023-12-28 14:55:16 +0330

    Edit

--- src/main/java/ir/maktabsharif101/oopjdbc/base/repository/impl/BaseEntityRepositoryImpl.java
+++ src/main/java/ir/maktabsharif101/oopjdbc/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -3,7 +3,8 @@ package ir.maktabsharif101.oopjdbc.base.repository.impl;
 import ir.maktabsharif101.oopjdbc.base.domain.Entity;
 import ir.maktabsharif101.oopjdbc.base.repository.BaseEntityRepository;
 
-public abstract class BaseEntityRepositoryImpl implements BaseEntityRepository {
+public abstract class BaseEntityRepositoryImpl
+        implements BaseEntityRepository {
     @Override
     public Entity[] findAll() {
         return new Entity[0];
