e136666a7704b36e704cd02104096d22780b0a1d
victus <zlotfi758@gmail.com>
2023-12-28 12:01:08 +0330

    create BaseEntityRepository interface and add method

---
+++ src/main/java/ir/maktabsharif101/oopjdbc/base/repository/BaseEntityRepository.java
@@ -0,0 +1,14 @@
+package ir.maktabsharif101.oopjdbc.base.repository;
+
+import ir.maktabsharif101.oopjdbc.base.domain.Entity;
+
+public interface BaseEntityRepository {
+
+    Entity[] findAll();
+    Entity findById(Long id);
+    void deleteById(Long id);
+    long count();
+    Entity sava(Entity entity);
+    Entity update(Entity entity);
+    boolean existsById(Long id);
+}
