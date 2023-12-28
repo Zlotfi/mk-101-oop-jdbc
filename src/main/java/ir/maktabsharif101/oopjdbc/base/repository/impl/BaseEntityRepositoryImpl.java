1b1cfbb5b328d8889892b6837e2ac26e566c0bee
victus <zlotfi758@gmail.com>
2023-12-28 12:04:40 +0330

    create BaseEntityRepositoryImpl class

--- src/main/java/ir/maktabsharif101/oopjdbc/base/repository/impl/BaseEntityRepositoryImpl.java
+++ src/main/java/ir/maktabsharif101/oopjdbc/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -3,39 +3,6 @@ package ir.maktabsharif101.oopjdbc.base.repository.impl;
 import ir.maktabsharif101.oopjdbc.base.domain.Entity;
 import ir.maktabsharif101.oopjdbc.base.repository.BaseEntityRepository;
 
-public class BaseEntityRepositoryImpl implements BaseEntityRepository {
-    @Override
-    public Entity[] findAll() {
-        return new Entity[0];
-    }
+public abstract class BaseEntityRepositoryImpl implements BaseEntityRepository {
 
-    @Override
-    public Entity findById(Long id) {
-        return null;
-    }
-
-    @Override
-    public void deleteById(Long id) {
-
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public Entity sava(Entity entity) {
-        return null;
-    }
-
-    @Override
-    public Entity update(Entity entity) {
-        return null;
-    }
-
-    @Override
-    public boolean existsById(Long id) {
-        return false;
-    }
 }
