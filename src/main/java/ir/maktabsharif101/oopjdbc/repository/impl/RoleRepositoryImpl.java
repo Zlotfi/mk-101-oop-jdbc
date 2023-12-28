c1135a37fbb5ba07e7c29c10063419941b225e9f
victus <zlotfi758@gmail.com>
2023-12-28 14:48:37 +0330

    add SuppressWarnings annotations

---
+++ src/main/java/ir/maktabsharif101/oopjdbc/repository/impl/RoleRepositoryImpl.java
@@ -0,0 +1,8 @@
+package ir.maktabsharif101.oopjdbc.repository.impl;
+
+import ir.maktabsharif101.oopjdbc.base.repository.impl.BaseEntityRepositoryImpl;
+import ir.maktabsharif101.oopjdbc.repository.RoleRepository;
+@SuppressWarnings("unused")
+public class RoleRepositoryImpl extends BaseEntityRepositoryImpl
+        implements RoleRepository {
+}
