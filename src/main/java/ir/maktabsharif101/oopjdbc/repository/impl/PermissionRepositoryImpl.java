c8058cc44e79dacb743c274dbc97b43480d0fef1
victus <zlotfi758@gmail.com>
2023-12-28 14:29:29 +0330

    add implements method

---
+++ src/main/java/ir/maktabsharif101/oopjdbc/repository/impl/PermissionRepositoryImpl.java
@@ -0,0 +1,15 @@
+package ir.maktabsharif101.oopjdbc.repository.impl;
+
+import ir.maktabsharif101.oopjdbc.base.domain.Entity;
+import ir.maktabsharif101.oopjdbc.base.repository.impl.BaseEntityRepositoryImpl;
+import ir.maktabsharif101.oopjdbc.domain.Permission;
+import ir.maktabsharif101.oopjdbc.repository.PermissionRepository;
+
+public class PermissionRepositoryImpl extends BaseEntityRepositoryImpl
+        implements PermissionRepository {
+
+    @Override
+    public Permission findByName(String name) {
+        return null;
+    }
+}
