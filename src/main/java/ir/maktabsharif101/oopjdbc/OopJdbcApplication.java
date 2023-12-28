9a631cff18c0f67e236ba278c1dd58779174b1a1
victus <zlotfi758@gmail.com>
2023-12-28 21:50:53 +0330

    Edit

--- src/main/java/ir/maktabsharif101/oopjdbc/OopJdbcApplication.java
+++ src/main/java/ir/maktabsharif101/oopjdbc/OopJdbcApplication.java
@@ -1,13 +1,8 @@
 package ir.maktabsharif101.oopjdbc;
 
-import ir.maktabsharif101.oopjdbc.base.domain.Entity;
-import ir.maktabsharif101.oopjdbc.repository.impl.RoleRepositoryImpl;
-import ir.maktabsharif101.oopjdbc.repository.impl.UserRepositoryImpl;
 import ir.maktabsharif101.oopjdbc.service.PermissionService;
 import ir.maktabsharif101.oopjdbc.service.RoleService;
 import ir.maktabsharif101.oopjdbc.service.UserService;
-import ir.maktabsharif101.oopjdbc.service.impl.RoleServiceImpl;
-import ir.maktabsharif101.oopjdbc.service.impl.UserServiceImpl;
 import ir.maktabsharif101.oopjdbc.util.ApplicationContext;
 
 public class OopJdbcApplication {
