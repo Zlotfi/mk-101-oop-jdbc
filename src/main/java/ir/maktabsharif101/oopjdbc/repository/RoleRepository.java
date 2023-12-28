01d574ea3838a7ec9e42b8963ddc017f7328db3b
victus <zlotfi758@gmail.com>
2023-12-28 14:33:14 +0330

    delete method

--- src/main/java/ir/maktabsharif101/oopjdbc/repository/RoleRepository.java
+++ src/main/java/ir/maktabsharif101/oopjdbc/repository/RoleRepository.java
@@ -4,5 +4,5 @@ import ir.maktabsharif101.oopjdbc.base.repository.BaseEntityRepository;
 import ir.maktabsharif101.oopjdbc.domain.Role;
 
 public interface RoleRepository extends BaseEntityRepository {
-    Role[] findAllByName(String name);
+
 }
