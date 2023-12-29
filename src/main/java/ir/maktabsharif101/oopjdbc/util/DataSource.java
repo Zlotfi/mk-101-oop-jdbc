46a7fe1b09f4abe5b84d81d4bf7a288a1acf86bb
victus <zlotfi758@gmail.com>
2023-12-29 16:52:36 +0330

    add connection

--- src/main/java/ir/maktabsharif101/oopjdbc/util/DataSource.java
+++ src/main/java/ir/maktabsharif101/oopjdbc/util/DataSource.java
@@ -1,4 +1,27 @@
 package ir.maktabsharif101.oopjdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SuppressWarnings("unused")
 public class DataSource {
+
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(
+                    ApplicationProperties.DATABASE_URL,
+                    ApplicationProperties.DATABASE_USER,
+                    ApplicationProperties.DATABASE_PASSWORD
+            );
+        }catch (Exception e){
+            System.out.println("wrong database connection");
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
