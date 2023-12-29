686c8851e71b3dd47bfdc5efb364ccf6322ee27d
victus <zlotfi758@gmail.com>
2023-12-29 16:27:39 +0330

    add Database properties

--- src/main/java/ir/maktabsharif101/oopjdbc/util/ApplicationProperties.java
+++ src/main/java/ir/maktabsharif101/oopjdbc/util/ApplicationProperties.java
@@ -1,4 +1,10 @@
 package ir.maktabsharif101.oopjdbc.util;
 
 public class ApplicationProperties {
+
+    public static final String DATABASE_NAME = "public";
+    public static final String DATABASE_URL =
+            "jdbc:postgresql://localhost:5432/postgres?currentSchema=" + DATABASE_NAME;
+    public static final String DATABASE_USER = "postgres";
+    public static final String DATABASE_PASSWORD = "zl13801393#";
 }
