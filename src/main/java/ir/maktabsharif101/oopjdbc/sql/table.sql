3e885f8ff384723076d2d109de0f154e584531c2
victus <zlotfi758@gmail.com>
2023-12-29 11:45:01 +0330

    create table user3

---
+++ src/main/java/ir/maktabsharif101/oopjdbc/sql/table.sql
@@ -0,0 +1,14 @@
+CREATE TYPE UserType AS ENUM('ADMIN','CUSTOMER');
+
+CREATE TABLE IF NOT EXISTS user3 (
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    userName VARCHAR(50) NOT NULl UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    mobileNumber VARCHAR(50) NOT NULL,
+    age INTEGER NOT NULL,
+    createDateMillis BIGINT,
+    createDate timestamp with time zone,
+    userType UserType
+);
